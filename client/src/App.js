70735e9013351278177e6f637f155aae2d1e1a09
Pablo Gomila <pablogiphone@icloud.com>
2021-09-10 21:32:09 -0300

    update(setroom to default and showchat after login)

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css"
 import io from 'socket.io-client'
 import { useState } from 'react'
 import Chat from './Chat'
@@ -7,35 +7,34 @@ const socket = io.connect('http://localhost:3001')
 
 function App() {
   const [username, setUsername] = useState('')
-  const [room, setRoom] = useState('')
+  const [showChat, setShowChat] = useState(false);
+  const room = "Activa"
   const joinRoom = () => {
     if (username !== '' && room !== '') {
-      socket.emit('unirse', room)
+      socket.emit('unirse', room);
+      setShowChat(!showChat);
     }
   }
 
   return (
     <div className="App">
-      <h1>Entrar al chat</h1>
-      <input
-        type="text"
-        placeholder="Tu nombre aquí"
-        onChange={(e) => {
-          setUsername(e.target.value)
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Id de la clase"
-        onChange={(e) => {
-          setRoom(e.target.value)
-        }}
-      />
-      <button onClick={joinRoom}>Entrar</button>
-      <Chat socket={socket} username={username} room={room}/>
-{}
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Login</h3>
+          <input
+            type="text"
+            placeholder="Ingresa tu nombre aquí"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Ingresar</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </div>
-  )
+  );
 }
 
 export default App
